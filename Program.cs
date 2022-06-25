4325ae03a5a904420bc75665693c8d6af267ba51
elcanion <elcaniondev@gmail.com>
2022-06-24 22:39:51 -0300

    add email validation funcionality

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Write("Digite seu nome: ");
+﻿using System.Text.RegularExpressions;
+
+Console.Write("Digite seu nome: ");
 string? name = Console.ReadLine();
 Console.WriteLine($"Olá {name}");
 Console.Write("Digite o ano do seu nascimento: ");
@@ -7,4 +9,11 @@ while (!int.TryParse(Console.ReadLine(), out yearOfBirth)) {
     Console.Write("Digite um ano de nascimento válido:");
 }
 int age = DateTime.Now.Year - yearOfBirth;
-Console.Write($"Você nasceu em {yearOfBirth} e você tem {age} anos");
+Console.WriteLine($"Você nasceu em {yearOfBirth} e você tem {age} anos");
+Console.Write("Digite seu email: ");
+string email;
+Regex rx = new Regex(@"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$");
+while (!rx.IsMatch(email = Console.ReadLine())) {
+    Console.Write("Digite um email válido:");
+}
+Console.WriteLine($"Seu email é: {email}");
