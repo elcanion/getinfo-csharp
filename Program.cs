ad764e36d7f5a43cd0c1ab397e4f72e511082ca5
elcanion <elcaniondev@gmail.com>
2022-06-24 23:10:29 -0300

    add User and TestUser classes

--- Program.cs
+++ Program.cs
@@ -1,19 +1,43 @@
 ﻿using System.Text.RegularExpressions;
 
-Console.Write("Digite seu nome: ");
-string? name = Console.ReadLine();
-Console.WriteLine($"Olá {name}");
-Console.Write("Digite o ano do seu nascimento: ");
-int yearOfBirth;
-while (!int.TryParse(Console.ReadLine(), out yearOfBirth)) {
-    Console.Write("Digite um ano de nascimento válido:");
+public class User {
+
+    public User(string name, int yearOfBirth, int age, string email) {
+        Name = name;
+        YearOfBirth = yearOfBirth;
+        Age = age;
+        Email = email;
+    }
+    public string Name { get; }
+    public int YearOfBirth { get; }
+    public int Age { get; }
+    public string Email { get; }
 }
-int age = DateTime.Now.Year - yearOfBirth;
-Console.WriteLine($"Você nasceu em {yearOfBirth} e você tem {age} anos");
-Console.Write("Digite seu email: ");
-string email;
-Regex rx = new Regex(@"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$");
-while (!rx.IsMatch(email = Console.ReadLine())) {
-    Console.Write("Digite um email válido:");
+
+class TestUser {
+    static void Main() {
+
+        string? name;
+        Console.Write("Digite seu nome: ");
+        while (String.IsNullOrEmpty(name = Console.ReadLine())) {
+            Console.Write("Digite seu nome: ");
+        }
+        Console.Write("Digite o ano do seu nascimento: ");
+        int yearOfBirth;
+        while (!int.TryParse(Console.ReadLine(), out yearOfBirth)) {
+            Console.Write("Digite um ano de nascimento válido:");
+        }
+        int age = DateTime.Now.Year - yearOfBirth;
+        Console.Write("Digite seu email: ");
+        string? email;
+        Regex rx = new Regex(@"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$");
+        while (String.IsNullOrEmpty(email = Console.ReadLine()) || !rx.IsMatch(email)) {
+            Console.Write("Digite um email válido:");
+        }
+
+        var user = new User(name, yearOfBirth, age, email);
+        Console.WriteLine($"Olá {user.Name}");
+        Console.WriteLine($"Você nasceu em {user.YearOfBirth} e você tem {user.Age} anos");
+        Console.WriteLine($"Seu email é {user.Email}");
+    }
 }
-Console.WriteLine($"Seu email é: {email}");
