fb74515dba40180881bcdebdf694b82143f574aa
elcanion <beethovenssixth@gmail.com>
2022-06-24 21:41:51 -0300

    add a basic funcionality to get user name and year of birth

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Digite seu nome: ");
+string? name = Console.ReadLine();
+Console.WriteLine($"Olá {name}");
+Console.Write("Digite o ano do seu nascimento: ");
+int year;
+while (!int.TryParse(Console.ReadLine(), out year)) {
+    Console.Write("Digite um ano de nascimento válido:");
+}
+
+Console.Write($"Voce nasceu em {year}");
